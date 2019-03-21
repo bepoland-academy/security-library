4f88bfc3857e683699457232d2956256210a198e
Wiktor Krzyżanowski <wiktor.krzyanowski1@gmail.com>
2019-03-21 10:55:08 +0100

    Added user GUID after login response for FE app

--- src/main/java/pl/betse/beontime/model/User.java
+++ src/main/java/pl/betse/beontime/model/User.java
@@ -1,6 +1,5 @@
 package pl.betse.beontime.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.util.List;
@@ -8,7 +7,6 @@ import java.util.List;
 @Data
 public class User {
 
-    @JsonIgnore
     private String userId;
 
     private String email;
